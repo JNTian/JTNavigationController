1d166d011929d83b6d67a42caf69e595874d98d6
Xander Xu <xander@texchi.com>
2017-10-20 16:25:18 +0800

    add safe guard and rotation support

--- JTNavigationController/JTNavigationController/Classes/JTNavigationController.m
+++ JTNavigationController/JTNavigationController/Classes/JTNavigationController.m
@@ -34,7 +34,9 @@
 }
 
 - (void)pushViewController:(UIViewController *)viewController animated:(BOOL)animated {
-    
+    if (![self.navigationController isKindOfClass:[JTNavigationController class]]) {
+        return;
+    }
     viewController.jt_navigationController = (JTNavigationController *)self.navigationController;
     viewController.jt_fullScreenPopGestureEnabled = viewController.jt_navigationController.fullScreenPopGestureEnabled;
     
@@ -44,14 +46,11 @@
         backButtonImage = [UIImage imageNamed:kDefaultBackImageName];
     }
     
-    viewController.navigationItem.leftBarButtonItem = [[UIBarButtonItem alloc] initWithImage:backButtonImage style:UIBarButtonItemStylePlain target:self action:@selector(didTapBackButton)];
+    viewController.navigationItem.leftBarButtonItem = [[UIBarButtonItem alloc] initWithImage:backButtonImage style:UIBarButtonItemStylePlain target:viewController action:@selector(jt_didTapBackButton:)];
     
     [self.navigationController pushViewController:[JTWrapViewController wrapViewControllerWithViewController:viewController] animated:animated];
 }
 
-- (void)didTapBackButton {
-    [self.navigationController popViewControllerAnimated:YES];
-}
 
 -(void)dismissViewControllerAnimated:(BOOL)flag completion:(void (^)(void))completion{
     [self.navigationController dismissViewControllerAnimated:flag completion:completion];
@@ -65,7 +64,10 @@
 @implementation JTWrapViewController
 
 + (JTWrapViewController *)wrapViewControllerWithViewController:(UIViewController *)viewController {
-    
+    if (!viewController) {
+        NSLog(@"%s %d Warning: wrapViewControllerWithViewController:nil", __FUNCTION__, __LINE__);
+        return nil;
+    }
     JTWrapNavigationController *wrapNavController = [[JTWrapNavigationController alloc] init];
     wrapNavController.viewControllers = @[viewController];
     wrapNavController.view.frame = CGRectMake(0, 0, wrapNavController.view.frame.size.width, wrapNavController.view.frame.size.height);
@@ -149,6 +151,34 @@
     
 }
 
+#pragma mark - 横屏控制支持
+
+- (BOOL)shouldAutorotate {
+    if (self.jt_viewControllers.count > 0) {
+        UIViewController* root = [self.jt_viewControllers firstObject];
+        return [root shouldAutorotate];
+    }
+    
+    return [super shouldAutorotate];
+}
+
+- (UIInterfaceOrientationMask)supportedInterfaceOrientations {
+    if (self.jt_viewControllers.count > 0) {
+        UIViewController* root = [self.jt_viewControllers firstObject];
+        return [root supportedInterfaceOrientations];
+    }
+    
+    return [super supportedInterfaceOrientations];
+}
+
+- (UIInterfaceOrientation)preferredInterfaceOrientationForPresentation {
+    if (self.jt_viewControllers.count > 0) {
+        UIViewController* root = [self.jt_viewControllers firstObject];
+        return [root preferredInterfaceOrientationForPresentation];
+    }
+    
+    return [super preferredInterfaceOrientationForPresentation];
+}
 
 #pragma mark - UINavigationControllerDelegate
 
